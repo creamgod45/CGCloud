9b43e88d046003a8d2093198e87d270b64c796a7
creamgod45 <fuyin1054@gmail.com>
2025-06-05 00:58:21 +0800

    更新 UI 元件 新增 panel-field-card.js 支援動態更新元件 修正 shareable.js 特定 popup 會無法正常顯示 修正 index.scss dark 模式顯示文字樣式

---
+++ resources/js/components/panel-field-card.js
@@ -0,0 +1,24 @@
+function panelFieldCard() {
+    let panelFieldCards = document.querySelectorAll(".panel-field-card:not(.pfc-rendered)");
+    for (const pfc of panelFieldCards) {
+        pfc.classList.add("pfc-rendered");
+        let addressList = {
+            pfc_title: pfc.querySelector(".pfc-title"),
+            pfc_tooltip: pfc.querySelector(".pfc-title").tippy,
+            pfc_preview: pfc.querySelector(".pfc-preview"),
+            pfc_operator: pfc.querySelector(".pfc-operator"),
+        };
+
+        pfc.visualRemove = () => {
+
+        };
+        pfc.update = () => {
+
+        };
+        pfc.clone = (deep = false) => {
+            return pfc.cloneNode(deep);
+        };
+    }
+}
+
+document.addEventListener("DOMContentLoaded", panelFieldCard);
