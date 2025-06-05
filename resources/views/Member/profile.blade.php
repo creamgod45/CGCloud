b04ce5d73e129c20082b18833503480778b0e08c
creamgod45 <fuyin1054@gmail.com>
2025-06-06 02:01:18 +0800

    更新個人檔案

--- resources/views/Member/profile.blade.php
+++ resources/views/Member/profile.blade.php
@@ -53,10 +53,11 @@
                                 <form method="POST" onsubmit="return false;">
                                     <button id="sendMailVerifyCode"
                                             type="button"
-                                            class="btn btn-ripple btn-color7 btn-max btn-center ct"
+                                            class="btn btn-ripple btn-color7 btn-max btn-center ct tippyer" data-zindex="10000" data-placement="auto" data-trigger="manual" data-theme="danger" data-htmlable="true" data-content="<li class='flex flex-nowrap'>🌟請先點擊發送信件</li>"
                                             data-fn="profile.email.sendMailVerifyCode"
                                             data-token="{{(new CSRF("profile.email.sendMailVerifyCode"))->get()}}"
                                             data-target="#sendMailVerifyCodeResult"
+                                            data-action1="#MailCatcher"
                                     >發送驗證碼【驗證身份】
                                     </button>
                                     <div id="sendMailVerifyCodeResult"></div>
@@ -66,12 +67,13 @@
                                         <label for="MailCatcher"
                                                class="xxl:w-1/12 xl:w-2/12 lg:w-2/12 md:w-2/12 footer:w-3/12 sm:w-full xs:w-full us:w-full flex justify-start items-center">驗證身份</label>
                                         <input id="MailCatcher"
-                                               class="block form-solid xxl:w-9/12 xl:w-8/12 lg:w-8/12 md:w-8/12 footer:w-6/12 sm:w-full xs:w-full us:w-full"
+                                               class="block form-solid xxl:w-9/12 xl:w-8/12 lg:w-8/12 md:w-8/12 footer:w-6/12 sm:w-full xs:w-full us:w-full tippyer" data-zindex="10000" data-placement="auto" data-trigger="manual" data-theme="light" data-htmlable="true" data-content="<li class='flex flex-nowrap'>⭕必填項目</li><li class='flex flex-nowrap'>📨 正確的驗證碼</li><li class='flex flex-nowrap'>❌最大的長度為5</li>"
                                                type="text" placeholder="填入驗證身份用途的驗證碼" minlength="5"
                                                autocomplete="off" required>
                                         <div
                                             class="footer:px-5 xxl:w-2/12 xl:w-2/12 lg:w-2/12 md:w-2/12 footer:w-3/12 sm:w-full footer:mt-0 xs:w-full sm:mt-5 xs:mt-5 us:w-full us:mt-5 sm:px-0">
                                             <button type="button"
+                                                    id="FirstEmailVerifyCode"
                                                     class="btn btn-max btn-center btn-color7 btn-ripple ct"
                                                     data-fn="profile.email.verifyCode"
                                                     data-token="{{(new CSRF("profile.email.verifyCode"))->get()}}"
@@ -90,9 +92,7 @@
                                         <label for="email"
                                                class="xxl:w-1/12 xl:w-2/12 lg:w-2/12 md:w-2/12 footer:w-3/12 sm:w-full xs:w-full us:w-full flex justify-start items-center">電子信箱</label>
                                         <input id="email"
-                                               class="block form-solid xxl:w-9/12 xl:w-8/12 lg:w-8/12 md:w-8/12 footer:w-6/12 sm:w-full xs:w-full us:w-full"
-                                               type="email" maxlength="255" placeholder="填入新的電子郵件" name="email"
-                                               autocomplete="new-email" disabled required>
+                                               class="block form-solid xxl:w-9/12 xl:w-8/12 lg:w-8/12 md:w-8/12 footer:w-6/12 sm:w-full xs:w-full us:w-full tippyer" data-zindex="10000" data-placement="auto" data-trigger="manual" data-theme="light" data-htmlable="true" data-content="<li class='flex flex-nowrap'>⭕必填項目</li><li class='flex flex-nowrap'>🌟獨一無二電子信箱</li><li class='flex flex-nowrap'>❌最大的長度為255</li>" data-method="email"  name="email" type="email" maxlength="255" placeholder="填入新的電子郵件" autocomplete="new-email" disabled required>
                                         <div
                                             class="footer:px-5 xxl:w-2/12 xl:w-2/12 lg:w-2/12 md:w-2/12 footer:w-3/12 sm:w-full footer:mt-0 xs:w-full sm:mt-5 xs:mt-5 us:w-full us:mt-5 sm:px-0">
                                             <button type="button"
@@ -110,7 +110,7 @@
                                         <label for="verification"
                                                class="xxl:w-1/12 xl:w-2/12 lg:w-2/12 md:w-2/12 footer:w-3/12 sm:w-full xs:w-full us:w-full flex justify-start items-center">驗證碼</label>
                                         <input id="verification"
-                                               class="block form-solid xxl:w-11/12 xl:w-10/12 lg:w-10/12 md:w-10/12 footer:w-9/12 sm:w-full xs:w-full us:w-full"
+                                               class="block form-solid xxl:w-11/12 xl:w-10/12 lg:w-10/12 md:w-10/12 footer:w-9/12 sm:w-full xs:w-full us:w-full tippyer" data-zindex="10000" data-placement="auto" data-trigger="manual" data-theme="light" data-htmlable="true" data-content="<li class='flex flex-nowrap'>⭕必填項目</li><li class='flex flex-nowrap'>📨 正確的驗證碼</li><li class='flex flex-nowrap'>❌最大的長度為5</li>"
                                                type="text" minlength="5" placeholder="填入新的電子郵件寄送的驗證碼"
                                                name="verification" autocomplete="off" disabled required>
                                     </div>
@@ -121,6 +121,7 @@
                                             data-value1="#verification"
                                             data-value2="#email"
                                             data-value3="#sendMailVerifyCodeToken"
+                                            data-value4="#sendMailVerifyCode"
                                             data-result="#profileUpdateEmailResult"
                                             data-token="{{(new CSRF("profile.profilepost"))->get()}}"
                                             data-method="email"
@@ -254,64 +255,64 @@
                 </div>
 
             </div>
-            <h1 class="mt-5 my-2 text-2xl font-bold noto-serif-tc-black tracking-widest"><i
-                    class="fa-solid fa-table-list"></i>&nbsp;儲存的訂單</h1>
-            <div>
-                @php
-                    $popover = "PW_".\Illuminate\Support\Str::random();
-                    $tableOptions = new \App\View\Components\TableOption('',
-                        [
-                            [ "data" => 'id', "title" => "ID", "footer" => "ID" ],
-                            [ "data" => 'goto', "title" => "訪問", "footer" => "訪問", "className" => 'clickable', "orderable"=> false,"searchable"=> false, ],
-                            [ "data" => 'name', "title" => "名稱" , "footer" => "名稱" ],
-                            [ "data" => 'priceTotal', "title" => "總金額", "footer" => "總金額" ],
-                            [ "data" => 'sentToLine', "title" => "Line 客服通知", "footer" => "Line 客服通知" ],
-                            [ "data" => 'countItem', "title" => "商品總數量", "footer" => "商品總數量" ],
-                            [ "data" => 'expired_at', "title" => "過期時間", "footer" => "過期時間" ],
-                        ],
-                        'ServerSide',
-                        [
-                            "url" => route(RouteNameField::APICustomerOrderListPost->value),
-                            "type" => 'POST',
-                            "timeout" => 100000,
-                            "headers" => [
-                                "X-CSRF-TOKEN" => csrf_token()
-                            ]
-                        ],
-                        //scroll: true,
-                        responsive: true,
-                        //scroller: [
-                        //    "displayBuffer" => 9,
-                        //],
-                        scrollY: 450,
-                        fixedHeader: true,
-                        fixedFooter: true,
-                        paging: true,
-                        popover: $popover,
-                    );
-                @endphp
-                <x-popover-windows class="order-item-popover !hidden"
-                                   popover-title="檢視詳細訂單" :id="$popover"
-                                   :popover-options="new \App\View\Components\PopoverOptions()">
-                    <div class="shop-popover-placeholder placeholder placeholder-full-wh">
-                        <div class="shop-popover-loader" role="status">
-                            <svg aria-hidden="true"
-                                 class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-red-600"
-                                 viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                    fill="currentColor"/>
-                                <path
-                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                    fill="currentFill"/>
-                            </svg>
-                            <span class="sr-only">Loading...</span>
-                        </div>
-                    </div>
-                    <iframe class="order-iframe"></iframe>
-                </x-popover-windows>
-                <x-DataTable :table-option="$tableOptions"></x-DataTable>
-            </div>
+            {{--  <h1 class="mt-5 my-2 text-2xl font-bold noto-serif-tc-black tracking-widest"><i
+                     class="fa-solid fa-table-list"></i>&nbsp;儲存的訂單</h1>
+             <div>
+                 @php
+                     $popover = "PW_".\Illuminate\Support\Str::random();
+                     $tableOptions = new \App\View\Components\TableOption('',
+                         [
+                             [ "data" => 'id', "title" => "ID", "footer" => "ID" ],
+                             [ "data" => 'goto', "title" => "訪問", "footer" => "訪問", "className" => 'clickable', "orderable"=> false,"searchable"=> false, ],
+                             [ "data" => 'name', "title" => "名稱" , "footer" => "名稱" ],
+                             [ "data" => 'priceTotal', "title" => "總金額", "footer" => "總金額" ],
+                             [ "data" => 'sentToLine', "title" => "Line 客服通知", "footer" => "Line 客服通知" ],
+                             [ "data" => 'countItem', "title" => "商品總數量", "footer" => "商品總數量" ],
+                             [ "data" => 'expired_at', "title" => "過期時間", "footer" => "過期時間" ],
+                         ],
+                         'ServerSide',
+                         [
+                             "url" => route(RouteNameField::APICustomerOrderListPost->value),
+                             "type" => 'POST',
+                             "timeout" => 100000,
+                             "headers" => [
+                                 "X-CSRF-TOKEN" => csrf_token()
+                             ]
+                         ],
+                         //scroll: true,
+                         responsive: true,
+                         //scroller: [
+                         //    "displayBuffer" => 9,
+                         //],
+                         scrollY: 450,
+                         fixedHeader: true,
+                         fixedFooter: true,
+                         paging: true,
+                         popover: $popover,
+                     );
+                 @endphp
+                 <x-popover-windows class="order-item-popover !hidden"
+                                    popover-title="檢視詳細訂單" :id="$popover"
+                                    :popover-options="new \App\View\Components\PopoverOptions()">
+                     <div class="shop-popover-placeholder placeholder placeholder-full-wh">
+                         <div class="shop-popover-loader" role="status">
+                             <svg aria-hidden="true"
+                                  class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-red-600"
+                                  viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                 <path
+                                     d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                     fill="currentColor"/>
+                                 <path
+                                     d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                     fill="currentFill"/>
+                             </svg>
+                             <span class="sr-only">Loading...</span>
+                         </div>
+                     </div>
+                     <iframe class="order-iframe"></iframe>
+                 </x-popover-windows>
+                 <x-DataTable :table-option="$tableOptions"></x-DataTable>
+             </div> --}}
         </div>
     </main>
 @endsection
